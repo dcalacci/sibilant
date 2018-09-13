2bf481616b90260e171c8a2def7da23b5c2899c4
Dan Calacci <dan@dcalacci.net>
2018-09-13 15:57:30 -0400

    slightly better band pass

--- demo/demo.js
+++ demo/demo.js
@@ -1,32 +1,57 @@
-const Sibilant = require('sibilant-webaudio')
-const alert = require('alerts')
-const getUserMedia = require('getusermedia')
-const attachmediastream = require('attachmediastream')
-const $ = require('jquery')
+const Sibilant = require("../")
+const alert = require('alerts');
+const getUserMedia = require('getusermedia');
+const attachmediastream = require('attachmediastream');
+const $ = require('jquery');
 
-getUserMedia(function (err, stream) {
-  if (err) {
-    console.log('couldnt get user media!', err)
-    alert('Sibilant couldnt get your user media. give it access to audio and try again!')
-    throw err
-  } else {
-    console.log('got a stream!')
 
-    attachmediastream(stream, document.querySelector('#vid video'))
-    document.querySelector('#vid video').muted = true
-    var speakingEvents = new Sibilant(stream, {passThrough: false})
-    speakingEvents.bind('speaking', function () {
-      document.querySelector('#vid video').style.border = '10px solid #27ae60'
-      console.log('speaking!')
-    })
+navigator.getUserMedia (
+    // constraints
+    {
+        video: true,
+        audio: true
+    },
 
-    speakingEvents.bind('stoppedSpeaking', function (data) {
-      console.log('speaking event recorded!', data.start)
-      var start = new Date(data.start)
-      var end = new Date(data.end)
-      var duration = end - start
-      $('#info').prepend('You spoke for ' + duration + ' ms! <br>')
-      document.querySelector('#vid video').style.border = '10px solid #555'
-    })
-  }
-})
+    // successCallback
+    function(stream) {
+      // var video = document.querySelector('video');
+      // video.src = window.URL.createObjectURL(localMediaStream);
+      // video.onloadedmetadata = function(e) {
+      //     // Do something with the video here.
+      // };
+      attachmediastream(stream, document.querySelector('#vid video'));
+      document.querySelector('#vid video').muted = true;
+      var speakingEvents = new Sibilant(stream, {passThrough: false});
+
+      console.log("whoa!", speakingEvents.volumes);
+
+      document.querySelector('#button').addEventListener('click', function() {
+        console.log("boooop")
+        speakingEvents.resumeContext().then(() => {
+          console.log('Playback resumed successfully');
+        });
+      });
+        speakingEvents.bind('speaking', function () {
+            document.querySelector('#vid video').style.border = '10px solid #27ae60';
+            console.log('speaking!');
+        });
+
+        speakingEvents.bind('stoppedSpeaking', function (data) {
+            console.log('speaking event recorded!', data.start);
+            var start = new Date(data.start);
+            var end = new Date(data.end);
+            var duration = end - start;
+            $('#info').prepend('You spoke for ' + duration + ' ms! <br>');
+            document.querySelector('#vid video').style.border = '10px solid #555';
+        });
+    },
+
+    // errorCallback
+    function(err) {
+        if(err === 'PERMISSION_DENIED') {
+            alert('Sibilant couldnt get your user media. give it access to audio and try again!', err);
+            console.log("error", err);
+            // Explain why you need permission and how to update the permission setting
+        }
+    }
+);
--- sibilant.js
+++ sibilant.js
@@ -1,95 +1,145 @@
-const microevent = require('microevent')
-const _ = require('lodash')
+const microevent = require('microevent');
+const _ = require('lodash');
 
 // get audio context
-const AudioContextType = window.AudioContext || window.webkitAudioContext
+const AudioContextType = window.AudioContext || window.webkitAudioContext;
 
 // f_1 band for (most) human voice range
-var LOW_FREQ_CUT = 85
-var HIGH_FREQ_CUT = 583
-var bandPassMiddleFrequency = ((HIGH_FREQ_CUT - LOW_FREQ_CUT) / 2) + LOW_FREQ_CUT
-var bandPassFrequencyRange = HIGH_FREQ_CUT - bandPassMiddleFrequency
+var LOW_FREQ_CUT = 85;
+var HIGH_FREQ_CUT = 583;
+var bandPassMiddleFrequency = ((HIGH_FREQ_CUT - LOW_FREQ_CUT) / 2) + LOW_FREQ_CUT;
+//var bandPassFrequencyRange = HIGH_FREQ_CUT - bandPassMiddleFrequency;
+var Q = bandPassMiddleFrequency / (HIGH_FREQ_CUT - LOW_FREQ_CUT);
+
+function getMaxVolume (analyser, fftBins) {
+  var maxVolume = -Infinity;
+  analyser.getFloatFrequencyData(fftBins);
+
+  for(var i=4, ii=fftBins.length; i < ii; i++) {
+    if (fftBins[i] > maxVolume && fftBins[i] < 0) {
+      maxVolume = fftBins[i];
+    }
+  };
+
+  return maxVolume;
+}
+
 
 // bandpass filter from AudioContext
 function bandPassFilterNode (audioContext) {
-  var bandpass = audioContext.createBiquadFilter()
-  bandpass.type = 'bandpass'
-  bandpass.frequency.value = bandPassMiddleFrequency
-  bandpass.Q = bandPassFrequencyRange
-  return bandpass
+  var bandpass = audioContext.createBiquadFilter();
+  bandpass.type = 'bandpass';
+  bandpass.frequency.value = bandPassMiddleFrequency;
+  bandpass.Q = Q;
+  return bandpass;
 }
 
 function speakingDetectionNode (audioContext, analyser, threshold, emitter) {
-  var javascriptNode = audioContext.createScriptProcessor(2048, 1, 1)
-  var speakingTimes = []
-  var quietHistory = [] // only contains continuous 'quiet' times
-  var currentVolume = -Infinity
-  var volumes = []
+  var javascriptNode = audioContext.createScriptProcessor(2048, 1, 1);
+  var speakingTimes = [];
+  var quietHistory = []; // only contains continuous 'quiet' times
+  var currentVolume = -Infinity;
+  var volumes = [];
 
   var hasStoppedSpeaking = function () {
-    return (_.max(quietHistory) - _.min(quietHistory) > 500)
-  }
+    return (_.max(quietHistory) - _.min(quietHistory) > 250);
+  };
 
   javascriptNode.onaudioprocess = function () {
-    var fftBins = new Float32Array(analyser.frequencyBinCount)
-    analyser.getFloatFrequencyData(fftBins)
-    var maxVolume = _.max(_.filter(fftBins, function (v) { return v < 0 }))
-    currentVolume = maxVolume
+    var fftBins = new Float32Array(analyser.frequencyBinCount);
+    analyser.getFloatFrequencyData(fftBins);
+    //var maxVolume = _.max(_.filter(fftBins, function (v) { return v < 0 }));
+    //currentVolume = maxVolume;
+    currentVolume = getMaxVolume(analyser, fftBins);
     volumes.push({timestamp: Date.now(),
-                  vol: currentVolume})
-    emitter.trigger('volumeChange', currentVolume)
+                  vol: currentVolume});
+    emitter.trigger('volumeChange', currentVolume);
     // speaking, add the date to the stack, clear quiet record
     if (currentVolume > threshold) {
-      emitter.trigger('speaking')
-      speakingTimes.push(new Date())
-      quietHistory = []
+      emitter.trigger('speaking');
+      speakingTimes.push(new Date());
+      quietHistory = [];
     } else if (speakingTimes.length > 0) {
       if (hasStoppedSpeaking()) {
-        emitter.trigger('stoppedSpeaking', {'start': _.min(speakingTimes), 'end': _.max(speakingTimes), 'volumes': volumes})
-        volumes = []
-        speakingTimes = []
+        emitter.trigger('stoppedSpeaking', {'start': _.min(speakingTimes), 'end': _.max(speakingTimes), 'volumes': volumes});
+        volumes = [];
+        speakingTimes = [];
       } else {
-        quietHistory.push(new Date())
+        quietHistory.push(new Date());
       }
     }
-  }
-  return javascriptNode
+  };
+  return javascriptNode;
 }
 
-var Sibilant = function (element, options) {
-  options = options || {}
-  var self = this
-  // var useBandPass = (options.useBandPass || true)
-  var fftSize = (options.fftSize || 512)
-  var threshold = (options.threshold || -40)
-  var smoothing = (options.smoothing || 0.2)
-  var passThrough = (options.passThrough || false)
-  var audioContext = new AudioContextType()
+var audioContext = null;
 
-  var analyser = audioContext.createAnalyser()
-  analyser.fftSize = fftSize
-  analyser.smoothingTimeConstant = smoothing
+class Sibilant {
+  constructor (stream, options) {
+    options = options || {};
+    var self = this;
+    // var useBandPass = (options.useBandPass || true)
+    this.fftSize = (options.fftSize || 512);
+    this.threshold = (options.threshold || -40);
+    this.smoothing = (options.smoothing || 0.2);
+    this.passThrough = (options.passThrough || false);
 
-  var audioSource = null
-  // assume webRTC for now
-  if (element instanceof HTMLAudioElement || element instanceof HTMLVideoElement) {
-    audioSource = audioContext.createMediaElementSource(element)
-  } else {
-    audioSource = audioContext.createMediaStreamSource(element)
+
+    console.log("middle freq:", bandPassMiddleFrequency);
+    console.log("range / Q:", Q);
+
+    // Ensure that just a single AudioContext is internally created
+    this.audioContext = options.audioContext || audioContext || new AudioContextType();
+
+    this.sourceNode = null;
+    this.fftBins = null;
+    this.analyser = null;
+
+    this.getStream(stream);
+
+    this.analyser = this.audioContext.createAnalyser();
+    this.analyser.fftSize = this.fftSize;
+    this.analyser.smoothingTimeConstant = this.smoothing;
+    this.fftBins = new Float32Array(this.analyser.frequencyBinCount);
+
+    this.audioSource = this.sourceNode;
+
+    var speakingNode = speakingDetectionNode(this.audioContext, this.analyser, this.threshold, this);
+    var bandPassNode = bandPassFilterNode(this.audioContext);
+    this.audioSource.connect(this.analyser);
+    if (this.passThrough) {
+      console.log('passing through', stream);
+      this.analyser.connect(this.audioContext.destination);
+    }
+    this.analyser.connect(bandPassNode);
+    bandPassNode.connect(speakingNode);
+    // needed for chrome onprocessaudio compatibility
+    speakingNode.connect(this.audioContext.destination);
   }
-  var speakingNode = speakingDetectionNode(audioContext, analyser, threshold, self)
-  var bandPassNode = bandPassFilterNode(audioContext)
-  audioSource.connect(analyser)
-  if (passThrough) {
-    console.log('passing through', element)
-    analyser.connect(audioContext.destination)
+
+  getStream (stream) {
+    if (stream.jquery) stream = stream[0];
+    if (stream instanceof HTMLAudioElement || stream instanceof HTMLVideoElement) {
+      //Audio Tag
+      this.sourceNode = this.audioContext.createMediaElementSource(stream);
+//      if (typeof play === 'undefined') play = true;
+      this.threshold = this.threshold || -50;
+    } else {
+      //WebRTC Stream
+      this.sourceNode = this.audioContext.createMediaStreamSource(stream);
+      this.threshold = this.threshold || -50;
+    }
   }
-  analyser.connect(bandPassNode)
-  bandPassNode.connect(speakingNode)
-  // needed for chrome onprocessaudio compatibility
-  speakingNode.connect(audioContext.destination)
+
+  suspend () {
+    this.audioContext.suspend();
+  }
+
+  resume () {
+    this.audioContext.resume();
+  }
 }
 
-microevent.mixin(Sibilant)
+microevent.mixin(Sibilant);
 
-module.exports = Sibilant
+module.exports = Sibilant;
